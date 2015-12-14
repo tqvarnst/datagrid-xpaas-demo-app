9697ca3784ffbcc8ec6a99c1c7ebda96a341f671
tqvarnst <tqvarnst@redhat.com>
2015-12-14 10:14:30 +0100

    Initial commit

---
+++ src/main/java/org/jboss/infinispan/demo/Config.java
@@ -0,0 +1,33 @@
+package org.jboss.infinispan.demo;
+
+import javax.enterprise.inject.Produces;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.jboss.infinispan.demo.model.Task;
+
+/**
+ * This class produces configured cache objects via CDI
+ *  
+ * @author tqvarnst
+ *
+ */
+public class Config {
+
+	
+	/**
+	 * DONE: Add a default Producer for org.infinispan.client.hotrod.RemoteCache<Long, Task> 
+	 * 		  using org.infinispan.client.hotrod.configuration.ConfigurationBuilder
+	 * 		  and org.infinispan.client.hotrod.RemoteCacheManager
+	 * 
+	 * @return org.infinispan.client.hotrod.RemoteCache<Long, Task>
+	 */
+	@Produces
+	public RemoteCache<Long, Task> getRemoteCache() {
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.addServer().host("localhost").port(11322);
+		return new RemoteCacheManager(builder.build(), true).getCache("default");
+	}
+
+}
